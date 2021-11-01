8009b61cd6ca39eef87f05658059e9fa3d251f38
Prashant K. Jha <pjha.sci@gmail.com>
2021-11-01 03:11:16 +0000

    update

---
+++ docs/docs/projects/project2/TumModel.m
@@ -0,0 +1,7 @@
+function [g, f, t] = TumModel(x, params)
+% Write function that returns discret solution of g and f as well as
+% discrete time vector t
+% 
+% x is a vector of optimization parameters
+% params is a struct that contains all other parameters
+end
